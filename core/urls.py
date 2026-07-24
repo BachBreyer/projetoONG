63d15565bb26ec35f3a0d05bac97ef75fc876117
joaonass <joaonass04@gmail.com>
2026-07-24 17:00:05 -0300

    responsividade na pagina de voluntários

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('historia/', views.historia, name='historia'),
     path('eventos/', views.eventos, name='events'),
-    path('doacoes/', views.doacoes, name='doacoes'),
     path('voluntariado/', views.voluntariado, name='voluntariado'),
     path("depoimentos/", views.testimonials_page, name="testimonials"),
     path('transparencia/', views.transparencia, name='transparencia'),
